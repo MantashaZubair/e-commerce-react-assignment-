126fdd51edda460f1d66d3c0055c53dda22650ce
Mantasha Zubair <98265444+MantashaZubair@users.noreply.github.com>
2023-01-14 03:06:04 -0800

    remove useLocation

--- src/Error.js
+++ src/Error.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom'
+
 
 
 const Error = () => {
-  const location = useLocation()
+  
   return (
     <div>
-      <h1>Opps! {location.pathname} only create Home.js not found</h1>
+      <h1>Opps! not found</h1>
     </div>
   )
 }
