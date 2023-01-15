ece8d718cdcc2fb2016e879fab172cf8940e380f
MantashaZubair <zmantasha3@gmail.com>
2023-01-14 16:18:19 -0800

    modified

--- src/components/Slider/card/Rating.js
+++ src/components/Slider/card/Rating.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar,faStarHalfStroke,fas } from '@fortawesome/free-solid-svg-icons';
+import { faStar,faStarHalfStroke} from '@fortawesome/free-solid-svg-icons';
+// import { faStar as lightStar} from '@fortawesome/free-regular-svg-icons';
+import { faStar as lightStar} from '@fortawesome/fontawesome-free-regular';
 export default function Rating(props) {
     const {rating, numreview}=props
     
@@ -8,11 +10,11 @@ export default function Rating(props) {
     <>
     <div className="rating">
   
-    <span>  <FontAwesomeIcon icon={ rating>=1 ?faStar : rating>=0.5?  faStarHalfStroke: fas}/></span>
-    <span>  <FontAwesomeIcon icon={ rating>=2 ?faStar : rating>=1.5?  faStarHalfStroke: fas}/></span>
-    <span>  <FontAwesomeIcon icon={ rating>=3 ?faStar : rating>=2.5?  faStarHalfStroke:fas}/></span>
-    <span>  <FontAwesomeIcon icon={ rating>=4 ?faStar : rating>=3.5?  faStarHalfStroke: fas }/></span>
-    <span>  <FontAwesomeIcon icon={ rating>=5 ?faStar : rating>=4.5?  faStarHalfStroke: fas}/></span>
+    <span>  <FontAwesomeIcon icon={ rating>=1 ?faStar : rating>=0.5?  faStarHalfStroke: lightStar}/></span>
+    <span>  <FontAwesomeIcon icon={ rating>=2 ?faStar : rating>=1.5?  faStarHalfStroke: lightStar}/></span>
+    <span>  <FontAwesomeIcon icon={ rating>=3 ?faStar : rating>=2.5?  faStarHalfStroke:lightStar}/></span>
+    <span>  <FontAwesomeIcon icon={ rating>=4 ?faStar : rating>=3.5?  faStarHalfStroke: lightStar }/></span>
+    <span>  <FontAwesomeIcon icon={ rating>=5 ?faStar : rating>=4.5?  faStarHalfStroke: lightStar}/></span>
     <span>{numreview +"review"} </span>
 
     
